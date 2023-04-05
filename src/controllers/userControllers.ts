bad52c583904f2f86616f47b24e4ee5277d5b547
Ranbut <Ranbut_Beater@protonmail.com>
2023-04-05 16:20:38 -0300

    feat: Server Init

---
+++ src/controllers/userControllers.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { db } from "../config/database";
+
+export async function UserMe (req: Request, res: Response) {
+
+    try{
+
+    }
+    
+    catch(err){
+
+    }
+}
+
+export async function ShowRanking (req: Request, res: Response) {
+
+    try{
+
+    }
+    catch(err){
+
+  }
+};
